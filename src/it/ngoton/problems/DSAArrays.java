f96b8a502c7fdf8cae565ce0f67aeef217368aaa
LIGVN-003 <ligvn-003@LIGVN-003.local>
2025-08-22 17:19:54 +0700

    Arrays

---
+++ src/it/ngoton/problems/DSAArrays.java
@@ -0,0 +1,149 @@
+package it.ngoton.problems;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class DSAArrays {
+    /**
+     * Tìm số bị thiếu trong dãy số từ 1 - N
+     * Input: list[] = {1, 2, 4, 6, 3, 7, 8, 10, 5}
+     * Output: 9
+     * <u>
+     *     <li>Tính tổng của N số tự nhiên đầu tiên là Tổng = (1 + N) * N / 2 : (số hạng đầu + số hạng cuối) * số số hạng / 2
+     *     <li>Tạo biến sum để lưu trữ tổng các phần tử của mảng.
+     *     <li>Lặp lại mảng từ đầu đến cuối.
+     *     <li>Cập nhật giá trị của tổng thành tổng = tổng + mảng [i]
+     *     <li>In số còn thiếu dưới dạng Tổng – tổng
+     * </u>
+     */
+    static int findMissingNumber(int[] arr, int n) {
+        int sum = (1+n)*n/2;
+        for (int i = 0; i < n-1; i++) {
+            sum -= arr[i];
+        }
+        return sum;
+    }
+
+    /**
+     * Tìm số trùng lặp trong mảng
+     * Input: list[] = {1, 2, 4, 1}
+     * Output: 1
+     * <u>
+     *     <li>Tạo một mảng tần số có kích thước N và khởi tạo nó bằng 0. (Có thể thay bằng Set)
+     *     <li>Duyệt qua mảng.
+     *     <li>Nếu tần số của phần tử là 0 thì tăng nó lên 1, nếu khác 0 thì phần tử đó là câu trả lời cần tìm.
+     * </u>
+     */
+    static int findDuplicateNumber(int[] arr) {
+        int n = arr.length;
+        int[] freq = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (freq[arr[i]-1] == 1) {
+                return arr[i];
+            }
+            freq[arr[i]-1] = 1;
+        }
+        return -1;
+    }
+
+    /**
+     * Tìm số trùng lặp trong dãy số từ 1 - N, gồm n + 1 số. Vì có nhiều hơn n phần tử nên ít nhất 1 số bị lặp lại
+     * Input: list[] = {3,1,3,4,2}
+     * Output: 3
+     * <u>
+     *     <li>Xem mảng như một danh sách liên kết ảo: Mỗi phần tử nums[i] là một "con trỏ" trỏ đến nums[nums[i]]
+     *     <li>Do có số lặp lại ⇒ sẽ tạo thành một chu trình (cycle).
+     *     <li>Floyd’s Tortoise and Hare (thuật toán phát hiện chu trình) sẽ giúp tìm điểm lặp.
+     * </u>
+     */
+    static int findDuplicateFloyd(int[] nums) {
+        int tortoise = nums[0];
+        int hare = nums[0];
+
+        // Phase 1: Find the intersection point (điểm giao nhau)
+        do {
+            tortoise = nums[tortoise]; // di chuyển 1 bước mỗi lần
+            hare = nums[nums[hare]]; // di chuyển 2 bước mỗi lần
+        } while (tortoise != hare);
+
+        // Phase 2: Find the entry point of the cycle (the duplicate) (điểm bắt đầu chu trình)
+        tortoise = nums[0]; // đưa 1 con trỏ về đầu mảng
+        while (tortoise != hare) {
+            // di chuyển từng bước 1 tới điểm bắt đầu chu trình
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+        // Khoảng cách từ head đến entry point = khoảng cách từ intersection đến entry point
+        return hare; // The duplicate number
+    }
+
+    /**
+     * Tìm các cặp số có tổng = k.
+     * Input: list[] = {3,1,3,4,2,5,1}
+     * Output: 3 3, 1 5, 4 2, 2 4, 5 1
+     * <u>
+     *     <li>Tạo 1 Map chứa vị trí của các số. (Nếu dùng Arrays.sort sẽ làm thay đổi vị trí.)
+     *     <li>Duyệt qua mảng
+     *     <li>Kiểm tra tồn tại trong trong Map giá trị k - arr[i] (tức tồn tại số hạng ở vị trí khác)
+     * </u>
+     */
+    static void findPairsEqualsToK(int[] arr, int k) {
+        HashMap<Integer, Integer> elementIndexMap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            elementIndexMap.put(arr[i], i);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            // we have used elementIndexMap.get(X-arr[i])!=i to avoid using same element twice
+            if (elementIndexMap.get(k - arr[i]) != null && elementIndexMap.get(k - arr[i]) != i)
+            {
+                System.out.println(arr[i] + " " + (k - arr[i]));
+            }
+        }
+    }
+
+    /**
+     * Xóa các số trùng lặp, thay = 0 vào cuối mảng. Không extra space. (Array được phép sort)
+     * Input: list[] = {3,1,3,4,2,5,1}
+     * Output: [1,2,3,4,5,0,0]
+     * <u>
+     *     <li>Sort mảng ban đầu. (Nếu phải giữ nguyên thứ tự thì dùng Set)
+     *     <li>Áp dụng two pointers, con trỏ j lưu các giá trị không trùng
+     *     <li>Duyệt mảng
+     *     <li>Cập nhật lại giá trị của mảng,
+     *     nếu giá trị là duy nhất thì thêm tiếp vào vị trí nums[j+1] (j bắt đầu từ 0), thay giá trị nums[i] hiện tại = 0
+     * </u>
+     * Không extra space, giữ order -> O(n2) : 2 loop
+     * Không extra space, k giữ order -> O(nlogn) : Sort array
+     * Extra space, giữ order -> O(n) : Set
+     */
+    static int[] removeDuplicates(int[] nums) {
+        Arrays.sort(nums);
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i]; // Move the unique element to the next position
+                nums[i] = 0; // Update the current element to 0
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {1, 2, 4, 6, 3, 7, 8, 10, 5};
+        System.out.println("Missing: " + findMissingNumber(list, list.length+1));
+        int[] list2 = {1,2,4,1};
+        System.out.println("Duplicate: " + findDuplicateNumber(list2));
+        int[] list3 = {3,1,3,4,2};
+        System.out.println("Duplicate Floyd: " + findDuplicateFloyd(list3));
+        int[] list4 = {3,1,3,4,2,5,1};
+        System.out.println("Find pairs:");
+        findPairsEqualsToK(list4, 6);
+        int[] list5 = {3,1,3,4,2,5,1};
+        System.out.println("Remove duplicates:");
+        int[] arr = removeDuplicates(list5);
+        for (int i : arr) {
+            System.out.print(i);
+        }
+    }
+}
